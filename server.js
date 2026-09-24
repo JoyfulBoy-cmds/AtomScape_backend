2e5c5f5da5e59e8c2d014e947407638ad568eba1
JoyfulBoy-cmds <kollaansh@gmail.com>
2026-09-23 23:20:45 -0400

    Update cookie settings for security and sameSite

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.use(session({
 
     cookie: {
         httpOnly: true,
-        sameSite: 'lax',
-        secure: false,
+        sameSite: 'none',
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }));
